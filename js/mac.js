138e1826bfada1657cb5634d39fc7894879ec795
arifurrahman5233 <arifurrahman5233@gmail.com>
2021-08-21 22:02:24 +0600

    promo

--- js/mac.js
+++ js/mac.js
@@ -1,44 +1,44 @@
-// base Amount
+//-- base Amount----//
 const baseAmount = document.getElementById('basePrice');
 
-// Memory
+//---- Memory------//
 
 const memoryStorage8 = document.getElementById('memory8gb');
 const memoryStorage16 = document.getElementById('memory16gb');
 
-// storage
+// ----storage-----//
 const storage256 = document.getElementById('storage256gb')
 const storage512 = document.getElementById('storage512gb')
 const storage1 = document.getElementById('storage1tb')
 
-// shipping
+//-- shipping----//
 const freeDelivery0 = document.getElementById('freeDelivery');
 const urgentDelivery20 = document.getElementById('urgentDelivery');
 
-// total
+//---- total----//
 const total = document.getElementById('totalCost');
 const totalAmount = document.getElementById('totalPrice');
 
 
-// Memory update
+//--- Memory update---//
 const memoryCost8gb = document.getElementById('memoryCost');
 const memoryCost16gb = document.getElementById('memoryCost')
 
-// storage update
+//--- storage update----//
 const storageCost256gb = document.getElementById('storageCost');
 const storageCost512gb = document.getElementById('storageCost');
 const storageCost1TB = document.getElementById('storageCost');
  
-// shipping update
+//---- shipping update---//
 
 const deliveryCost0 = document.getElementById('deliveryCost');
 const deliveryCost20 = document.getElementById('deliveryCost');
 
-// base update
+//--- base update---//
 
 const baseFixed = document.getElementById('basePrice');
 
-// total update 
+//---- total update----// 
 function updateTotal() {
     const memoryPrice = Number(memoryCost16gb.innerText);
     const storagePrice = Number(storageCost256gb.innerText);
@@ -49,8 +49,27 @@ function updateTotal() {
     const grandTotal = memoryPrice + baseTotal + storagePrice +storageprice1TB +  shippingPrice;
     total.innerText = grandTotal;
     totalAmount.innerText = grandTotal;
+
+//----Promo apply---//
+document.getElementById('promoApply').addEventListener('click', function () {
+    const totalPrice = document.getElementById('totalPrice');
+    const totalPriceText = totalPrice.innerText;
+    const totalPriceNumeric = grandTotal;
+
+    const promoInput = document.getElementById('promoCode');
+    const promoInputText = promoInput.value;
+
+    if (promoInputText == 'stevekaku') {
+        const discountPrice = totalPriceNumeric * 0.2;
+        const totalPriceAmount = grandTotal - discountPrice;
+        totalPrice.innerText = totalPriceAmount;
+    }
+    else {
+        totalPrice.innerText = totalamount;
+    }
+});
 }
-// Memory price handle
+//--- Memory price handle-----//
 
 memoryStorage8.addEventListener('click', function () {
     memoryCost8gb.innerText = 0;
@@ -63,7 +82,7 @@ memoryStorage16.addEventListener('click', function () {
 });
  
 
-// storage price handle
+//---- storage price handle----//
 storage256.addEventListener('click', function () {
     storageCost256gb.innerText = 0;
     updateTotal()
@@ -78,7 +97,7 @@ storage1.addEventListener('click', function () {
     updateTotal()
 })
 
-// shipping price handle
+//---- shipping price handle-----//
 freeDelivery0.addEventListener('click', function () {
     deliveryCost0.innerText = 0;
     updateTotal()
@@ -88,11 +107,8 @@ urgentDelivery20.addEventListener('click', function () {
     updateTotal()
 })
 
-// base price handle
+//---- base price handle-----//
 baseAmount.addEventListener('click', function () {
     baseFixed.innerText = 1299;
     updateTotal()
-})
-
-
-
+});
